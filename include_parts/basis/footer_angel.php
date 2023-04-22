44a2296b3f1e119ddec816b7d201c503569aa524
Christina-Saitoh <christiina.keiko@gmail.com>
2023-04-22 19:59:30 +0900

    sustain

--- include_parts/basis/footer_angel.php
+++ include_parts/basis/footer_angel.php
@@ -1,4 +1,4 @@
 <div class="float_wrapper">
-	<p><small>The Arknet Angel &copy; for poeple who ask for the truth,for all nations and kinds,for all humans,angels and Gods</small></p>
+	<p><small>The Arknet Angel &copy; for poeple who ask for the truth,for all nations and kinds,for all humans,angels</small></p>
 </div>
 
